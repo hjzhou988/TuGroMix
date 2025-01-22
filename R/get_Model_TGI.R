9707e7d0a8b4fe5a10601c5fbe7fcee5e382fc2e
Huajun Zhou <huajunzhou@CrownBiodeMacBook-Pro.local>
2025-01-22 10:07:45 +0800

    v1.2.1, add option of geometric or arithmetic mean, and option of specifying CI boundary

--- R/get_Model_TGI.R
+++ R/get_Model_TGI.R
@@ -3,22 +3,24 @@
 #' @param tv.data tumor volume data in long format, containing columns "Group","Mouse","Day","TV"
 #' @param ref.group the reference group in eGR ratio or difference
 #' @param def definition of TGI. 1: TV_trt/TV_ctl, 2: DeltaTV_trt/DeltaTV_ctl, 3: 1-DeltaTV_trt/DeltaTV_ctl, 4:RTV_trt/RTV_ctl, 5: 1-TV_trt/TV_ctl, 6: 1-RTV_trt/RTV_ctl
+#' @param type the type of the mean of TGI, choose from "arithmetic" and "geometric".
 #' @param group.id.var The name of the "group" column of your input data. Required.
 #' @param mouse.id.var The name of the "mouse.id" column of your input data. Required.
 #' @param time.var The name of the "day" column of your input data. Required.
 #' @param tv.var  The name of the "tumor volume" column of your input data. Required.
 #' @param ci a logical variable to do bootstrapping for confidence interval
-#'
+#' @param bound a number for the confidence interval boundary. Suggest 90 or 95.
 #' @return a tibble containing TGI for each treatment group
 #'
 #' @importFrom magrittr %>%
 #' @export
 #'
-get_Model_TGI = function(tv.data,ref.group,def=6, group.id.var, mouse.id.var,time.var,tv.var,ci = F){
+get_Model_TGI = function(tv.data,ref.group,type = c("geometric","arithmetic"),def=6, group.id.var, mouse.id.var,time.var,tv.var,ci = F,bound = 90){
+  type = match.arg(type)
   # proj_lst = split(tv_long, tv_long$Model)
   # proj_lst = split(tv.data.screen, tv.data.screen$Model)
   # tv.data = proj_lst2[[1]];ref.group="Vehicle";group.id.var="Trt";mouse.id.var="Mouse.ID";time.var="Day";tv.var="TV"
-  # tv.data = readRDS("~/Documents/VCG/2024-8-1_example_data_for_tugromix_tgi_test_44.rds");ref.group = "Grp-1";def = 6;group.id.var = "Grp.Name";mouse.id.var = "Mouse";time.var = "Day";tv.var = "TV";ci = T
+  # tv.data = readRDS("~/Documents/VCG/2024-8-1_example_data_for_tugromix_tgi_test_syn2.rds");ref.group = "Grp-1";def = 6;group.id.var = "Grp.Name";mouse.id.var = "session_key";time.var = "Day";tv.var = "TV";ci = T
   tv.data = dplyr::rename(tv.data, Group = {{group.id.var}}, Mouse = {{mouse.id.var}}, Day = {{time.var}}, TV = {{tv.var}})
 
   tv.data = tv.data[,c("Group","Mouse","Day","TV")]
@@ -40,7 +42,7 @@ get_Model_TGI = function(tv.data,ref.group,def=6, group.id.var, mouse.id.var,tim
   # tgi_df=base::as.data.frame(base::do.call(base::rbind,base::lapply(grps,function(g) tgi_group(tv.data,ref.group,g,def=def))))
   # tgi_df$Model=proj
   tgi_df=dplyr::bind_rows(base::lapply(grps,function(g) {
-    tgi_group(tv.data,ref.group,g,def=def,ci = ci)
+    tgi_group(tv.data,ref.group,g,type = type,def=def,ci = ci,bound = bound)
     })
   )
 
--- R/tgi_group.R
+++ R/tgi_group.R
@@ -3,8 +3,10 @@
 #' @param tv.data a data frame containing tumor volume data in long format
 #' @param ref.group the reference group
 #' @param grp the group to calculate TGI for
+#' @param type the type of the mean of TGI, choose from "arithmetic" and "geometric".
 #' @param def definition of TGI
 #' @param ci whether to do 1000 times bootstrapping to determine the 95 confidence interval for the calculated TGI.
+#' @param bound a number for the confidence interval boundary. Suggest 90 or 95.
 # #' @param group.id.var The name of the "group" column of your input data. Required.
 # #' @param mouse.id.var The name of the "mouse.id" column of your input data. Required.
 # #' @param time.var The name of the "day" column of your input data. Required.
@@ -24,8 +26,9 @@
 #'
 #'
 
-tgi_group = function(tv.data,ref.group,grp,def=6, ci = F){ #,group.id.var,mouse.id.var,time.var,tv.var
+tgi_group = function(tv.data,ref.group,grp,type = c("geometric","arithmetic"),def=6, ci = F, bound = 90){ #,group.id.var,mouse.id.var,time.var,tv.var
 
+  type = match.arg(type)
   # tv.data = tv.data; ref.group = ref.group; grp = "Dosing";def = 3
   # tv.data = dplyr::rename(tv.data, Group = {{group.id.var}}, Mouse = {{mouse.id.var}}, Day = {{time.var}}, TV = {{tv.var}})
   #
@@ -36,7 +39,7 @@ tgi_group = function(tv.data,ref.group,grp,def=6, ci = F){ #,group.id.var,mouse.
   # tv.data$TV0 = ifelse(tv.data$TV0==0, 1, tv.data$TV0)
   # tv.data$RTV = tv.data$TV/tv.data$TV0
   # tv.data$DeltaTV = tv.data$TV - tv.data$TV0
-
+  # tv.data = tv.data; ref.group = ref.group; grp = "Grp-2";def = 6
 
   tv_var='TV'
   if(def %in% c(2,3)){
@@ -51,14 +54,22 @@ tgi_group = function(tv.data,ref.group,grp,def=6, ci = F){ #,group.id.var,mouse.
   # tv_wide_long = tv_wide_long  %>% mutate(RTV = TV/TV0,DeltaTV = TV-TV0)
   # tv.data = tv_wide_long
 
-  ds = tv.data[,] # %>% dplyr::filter(Group %in% c(ref.group,grp))
+  # ds = tv.data %>% dplyr::filter(Group %in% c(ref.group,grp))
+  ds = subset(tv.data,Group==ref.group | Group == grp)
   day_n=base::table(ds$Group,ds$Day)
   day_pct=day_n/day_n[,1]
   xx=base::apply(day_pct,2,function(v) all(v>=0.6))
   sel_day=base::max(base::as.numeric(base::names(xx[xx])),na.rm = T) # add na.rm = T, just in case it returns NA for sel_day
   ds=dplyr::filter(ds,Day==sel_day)
-  ms=base::tapply(ds[,tv_var,drop = T],ds$Group,mean,na.rm = T) # just in case there is NA in RTV.
+
+  if (type=="arithmetic"){
+    ms=tapply(ds[[tv_var]],ds$Group,mean,na.rm = T)
+  }else if(type=="geometric"){
+    ms=tapply(ds[[tv_var]],ds$Group, function(x) exp(mean(log(x+1),na.rm =T)))
+  }
+
   ms1=ms[-which(names(ms)==ref.group)]/ms[ref.group]
+
   if(def %in% c(3,5,6)) ms1 = 1-ms1 # added 6
   if(ci == F) list('Group'=grp,'TGI'=as.numeric(ms1),'Day'=sel_day) else {
     # tic()
@@ -67,14 +78,23 @@ tgi_group = function(tv.data,ref.group,grp,def=6, ci = F){ #,group.id.var,mouse.
       sampled = tapply(ds[[tv_var]], ds$Group, sample,replace = TRUE)
 
       # ms=base::tapply(ds.s[,tv_var,drop = T],ds.s$Group,mean)
-      ms = lapply(sampled, mean, na.rm = T)
+
+      if (type=="arithmetic"){
+        ms = lapply(sampled, mean, na.rm = T)
+      }else if(type=="geometric"){
+        ms = lapply(sampled, function(x) exp(mean(log(x+1),na.rm =T)))
+      }
+
       # ms.b=ms[-which(names(ms)==ref.group)]/ms[ref.group]
       ms.b=ms[[-which(names(ms)==ref.group)]]/ms[[ref.group]]
       if(def %in% c(3,5,6)) ms.b = 1-ms.b # added 6
       ms.b
     })
     # toc()
-    qts = stats::quantile(base::unlist(res),c(0.025, 0.975))
-    base::list('Group'=grp,'TGI'=as.numeric(ms1),'95_CI_lower_bound'= qts[[1]],'95_CI_upper_bound'= qts[[2]],'Day'=sel_day)
+    qts = stats::quantile(base::unlist(res),c((100-bound)/200, (bound + (100-bound)/2)/100))
+    out = base::list('Group'=grp,'TGI'=as.numeric(ms1),'3' = qts[[1]],'4'= qts[[2]],'Day'=sel_day)
+    names(out)[3]=paste0(bound,"_CI_lower_bound")
+    names(out)[4]=paste0(bound,"_CI_upper_bound")
+    out
   }
 }
